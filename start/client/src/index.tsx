b1fc4d416b86afc588efeca21d0f41c4fbf2d1a5
Andrew Miracle <me@andrewmiracle.com>
2020-01-27 17:42:45 +0000

    implement resolvers

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,18 +1,31 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-// import gql from 'graphql-tag';
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles'
+import { resolvers, typeDefs } from './resolvers'
+import gql from 'graphql-tag';
+import Login from './pages/login';
 
+
+const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+        isLoggedIn @client
+    }
+`;
+
+function IsLoggedIn() {
+    const { data } = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages /> : <Login />
+}
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-    uri: 'http://localhost:4000'
-});
+// const link = new HttpLink({
+//     uri: 'http://localhost:4000'
+// });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache,
@@ -20,16 +33,27 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
         uri: 'http://localhost:4000/graphql',
         headers: {
             authorization: localStorage.getItem('token')
-        }
-    })
+        },
+    }),
+    typeDefs,
+    resolvers
+});
+
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem('token'),
+        cartItems: []
+    },
 })
+
+
 
 // ... instantiate the client context tree
 
 injectStyles();
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Pages />
+        <IsLoggedIn />
     </ApolloProvider>,
     document.getElementById('root')
 );
--- start/client/src/resolvers.tsx
+++ start/client/src/resolvers.tsx
@@ -1,1 +1,54 @@
-export default {}
+import gql from 'graphql-tag';
+import { ApolloCache } from 'apollo-cache';
+import * as GetCartItemTypes from './pages/__generated__/GetCartItems'
+import * as LaunchTileTypes from './pages/__generated__/LaunchTile'
+import { Resolvers } from 'apollo-client'
+import { GET_CART_ITEMS } from './pages/cart'
+
+export const typeDefs = gql`
+    extend type Query {
+        isLoggedIn: Boolean!
+        cartItems: [ID!]!
+    }
+    extend type Launch {
+        isInCart: Boolean!
+    }
+    extend type Mutation {
+        addOrRemoveFromCart(id: ID!): [ID!]!
+    }
+`;
+
+export const schema = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
+
+type ResolverFn = (
+    parent: any,
+    args: any,
+    { cache }: { cache: ApolloCache<any> }
+) => any;
+
+interface ResolverMap {
+    [field: string]: ResolverFn
+}
+
+interface AppResolvers extends Resolvers {
+    Launch: ResolverMap
+    // We will update this with our app's resolvers later
+}
+
+export const resolvers: AppResolvers = {
+    Launch: {
+        isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
+            const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({
+                query: GET_CART_ITEMS
+            });
+            if (queryResult) {
+                return queryResult.cartItems.includes(launch.id)
+            }
+            return false;
+        }
+    },
+}
