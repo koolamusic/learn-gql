694baeacd36ad404d65a96ff710de0076677bee9
Andrew Miracle <me@andrewmiracle.com>
2020-03-01 07:13:12 +0000

    type relationships

--- robinwieruch.de/src/index.js
+++ robinwieruch.de/src/index.js
@@ -4,6 +4,35 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
+let users = {
+	1: {
+		id: '1',
+		username: 'Andrew Weruch',
+		messageIds: [ 22 ]
+	},
+	2: {
+		id: '2',
+		username: 'Dave Dvids',
+		messageIds: [ 2 ]
+	}
+};
+let messages = {
+	1: {
+		id: '1',
+		text: 'Hello World',
+		userId: '1'
+	},
+	2: {
+		id: '2',
+		text: 'Second Message World',
+		userId: '2'
+	},
+	3: {
+		id: '3',
+		text: 'Third Message',
+		userId: '2'
+	}
+};
 
 // initialize and use cors here
 app.use(cors());
@@ -13,17 +42,27 @@ const schema = gql`
 		me: User
 		users: [User!]
 		user(id: ID!): User
+		messages: [Message!]!
+		message(id: ID!): Message!
 	}
 
 	type User {
 		id: ID!
 		username: String!
+		messages: [Message!]
+	}
+
+	type Message {
+		id: ID!
+		text: String!
+		userId: String
+		user: User!
 	}
 `;
 
 const resolvers = {
 	Query: {
-		me: () => {
+		me: (_, __, { me }, info) => {
 			return me;
 		},
 		users: () => {
@@ -32,13 +71,39 @@ const resolvers = {
 		user: (parent, { id }) => {
 			console.log('HERSD======', id);
 			return users[id];
+		},
+		messages: () => {
+			return Object.values(messages);
+		},
+		message: (parent, { id }) => {
+			return messages[id];
+		}
+	},
+
+	User: {
+		username: (parent) => {
+			return parent.username + 3;
+		},
+		messages: (user) => {
+			console.log('XX----', user, messages);
+			return Object.values(messages).filter((message) => message.userId === user.id);
+		}
+	},
+
+	Message: {
+		user: (parent) => {
+			console.log('G999----', parent);
+			return users[parent.userId];
 		}
 	}
 };
 
 const server = new ApolloServer({
 	typeDefs: schema,
-	resolvers
+	resolvers,
+	context: {
+		me: users[1]
+	}
 });
 
 server.applyMiddleware({ app, path: '/electron' });
@@ -46,16 +111,3 @@ server.applyMiddleware({ app, path: '/electron' });
 app.listen({ port: 8000 }, () => {
 	console.log('Apollo Server on http://localhost:8000/graphql');
 });
-
-let users = {
-	1: {
-		id: '1',
-		username: 'Andrew Wieruch'
-	},
-	2: {
-		id: '2',
-		username: 'Dave Davids'
-	}
-};
-
-const me = users[1];
